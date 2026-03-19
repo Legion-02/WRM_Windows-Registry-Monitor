41e865ea9ea19c80d7fb5c49dcbe13ec6b04b830
Legion Commander <anushp280204@gmail.com>
2026-03-19 09:27:23 +0530

    Refactor log_change function for improved logging

--- logger.py
+++ logger.py
@@ -1,11 +1,4 @@
 from datetime import datetime
-
-def log_change(action, path, key, old, new):
-    with open("logs.txt", "a") as f:
-        f.write(f"\n[{datetime.now()}]\n")
-        f.write(f"Action: {action}\n")
-        f.write(f"Path: {path}\n")
-        f.write(f"Key: {key}\n")
-        f.write(f"Old Value: {old}\n")
-        f.write(f"New Value: {new}\n")
-        f.write("-------------------------\n")
+def log_change(a,p,k,o,n,s,r):
+    with open("logs.txt","a") as f:
+        f.write(f"[{datetime.now()}]\n{a} {p} {k}\nSeverity:{s}\nReason:{r}\n\n")
