bf29b3e0545fbd1c95fec642c9e58dc9ee3bed3b
Legion Commander <anushp280204@gmail.com>
2026-03-19 09:26:38 +0530

    Refactor detect_changes function for conciseness

--- detector.py
+++ detector.py
@@ -1,17 +1,8 @@
-def detect_changes(old, new):
-    changes = []
-
-    old_keys = set(old.keys())
-    new_keys = set(new.keys())
-
-    for key in new_keys - old_keys:
-        changes.append(("ADDED", key, None, new[key]))
-
-    for key in old_keys - new_keys:
-        changes.append(("REMOVED", key, old[key], None))
-
-    for key in old_keys & new_keys:
-        if old[key] != new[key]:
-            changes.append(("MODIFIED", key, old[key], new[key]))
-
+def detect_changes(old,new):
+    changes=[]
+    ok=set(old.keys()); nk=set(new.keys())
+    for k in nk-ok: changes.append(("ADDED",k,None,new[k]))
+    for k in ok-nk: changes.append(("REMOVED",k,old[k],None))
+    for k in ok&nk:
+        if old[k]!=new[k]: changes.append(("MODIFIED",k,old[k],new[k]))
     return changes
